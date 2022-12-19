7dc2c70529f4bcc09fec15cbf4c9e58b556f75fe
lynelldenise <113837831+lynelldenise@users.noreply.github.com>
2022-12-20 03:43:24 +0800

    Update tg_bot.py

--- src/tg_bot.py
+++ src/tg_bot.py
@@ -4,7 +4,7 @@ import re
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-
+#Connect to database
 con = sqlite3.connect('chatbotDatabase.db', check_same_thread=False)
 cur = con.cursor()
 
@@ -14,15 +14,22 @@ class TgBot:
         self.config = config
         self.email_fetcher = email_fetcher
         self.message_handler = message_handler
-
+        
+    #Create updater and pass it to your bot's token
+    #Use use_context=True to use the new context based callbacks
     def start(self):
         updater = Updater(self.config.credentials_config.telegram_config.token, use_context=True)
+        #Get the dispatcher to register handlers
         dp = updater.dispatcher
-
+        
+        #Command - answer in Telegram
         dp.add_handler(CommandHandler("start", self._fetch_unread_emails_bg))
+        #Noncommand message - echo the message on Telegram
         dp.add_handler(MessageHandler(Filters.text, self.message_handler))
+        #Log all errors 
         dp.add_error_handler(error)
-
+        
+        #Start the bot
         updater.start_polling()
         updater.idle()
 
@@ -32,7 +39,7 @@ class TgBot:
                 self.config.email_fetching_config.folder,
                 lambda msg_to_display: self._display_message(update, msg_to_display)
             )
-
+    #Build a thread using the function and arguments given then start the thread
     def _fetch_unread_emails_bg(self, update, context):
         thread = threading.Thread(target=self._fetch_unread_emails, args=(update, context)) 
         thread.start()
@@ -42,7 +49,8 @@ class TgBot:
         h_m_id = re.sub('[(),\'\']', '', h_m_id )
         reply_to = (str(msg.headers.get('reply-to')))
         reply_to = re.sub('[(),\'\']', '', reply_to )
-
+        
+        #Execute SQL query and save the changes
         cur.execute('''INSERT INTO tblMsgsLogs values (?, ?)''', (reply_to, h_m_id))
         con.commit()
 
